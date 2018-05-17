c515ecfe5ee4b829dc6414e6590e96d88225ae5a
Wenceslao Negrete <vampay@digitaldealers.mx>
2018-05-17 00:28:43 -0500

    CSS transitions

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 var modal = document.querySelector('.modal');
 var closeButton = document.querySelector('.modal .close');
 var anchors = document.querySelectorAll('.strip a');
-var items = document.querySelectorAll('.strip li');
 
 closeButton.addEventListener('click', function(e){
   e.preventDefault();
@@ -14,10 +13,4 @@ anchors.forEach(a => a.addEventListener('click', function(e){
   modalImg.src = img.src;
   modal.style.display = 'flex';
   TweenMax.from('.modal', 0.3, {opacity: 0, scale: 0.85});
-}));
-items.forEach(a => a.addEventListener('mouseover', function(e){
-  TweenMax.to(this, 0.3, {scale: 1.2});
-}));
-items.forEach(a => a.addEventListener('mouseleave', function(e){
-  TweenMax.to(this, 0.3, {scale: 1});
 }));
